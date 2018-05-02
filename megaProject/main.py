66d41df637fc9bdeae48586c34860d1021685c21
Kenedi231 <kalsinkolya@gmail.com>
2018-05-02 21:37:07 +0300

    update lab8, lab9

--- megaProject/main.py
+++ megaProject/main.py
@@ -1,34 +1,29 @@
 from module import Matrix
-A = Matrix.createMatrix(3, 3)
-B = Matrix.createMatrix(3, 3)
+A = Matrix(3, 3)
+B = Matrix(3, 3)
 print("A")
-Matrix.display(A)
+A.display()
 print("B")
-Matrix.display(B)
-print("Add")
-matrixAdd = Matrix.add(A, B)
-if matrixAdd != 1:
-    Matrix.display(matrixAdd)
-print("Sub")
-matrixSub = Matrix.sub(A, B)
-if matrixSub != 1:
-    Matrix.display(matrixSub)
-n = 4
-print("A * " + str(n))
-matrixComp = Matrix.compOfNumber(A, n)
-if matrixComp != 1:
-    Matrix.display(matrixComp)
-print("A * B")
-matrixTwoComp = Matrix.compTwoMatrix(A, B)
-if matrixTwoComp != 1:
-    Matrix.display(matrixTwoComp)
-print("Trans B")
-transMatrix = Matrix.transMatrix(B)
-Matrix.display(transMatrix)
-print("Det B")
-detMatrix = Matrix.detMatrix(B)
-if detMatrix != 1:
-    print(detMatrix)
-
-
-
+B.display()
+C = A + B
+print("C = A + B")
+if C != 1:
+    C.display()
+D = A - B
+print("D = A - B")
+if D != 1:
+    D.display()
+E = A * 2
+print("E = A * 2")
+if E != 1:
+    E.display()
+F = A * B
+print("F = A * B")
+if F != 1:
+    F.display()
+A.trans()
+print("A^T")
+A.display()
+detA = A.det()
+if detA != 'string':
+    print("Det A = " + str(detA))
--- megaProject/module.py
+++ megaProject/module.py
@@ -2,102 +2,96 @@ import random
 
 class Matrix:
     def __init__(self, s, p):
-        self.string = s
-        self.pillar = p
+        self.rows = s
+        self.columns = p
         self.matrix = []
-        for i in range(self.string):
+        for i in range(self.rows):
             self.matrix.append([])
-            for j in range(self.pillar):
-                self.matrix[i].append(random.randint(1, 101))
+            for j in range(self.columns):
+                self.matrix[i].append(random.randint(0, 1))
+    def display(self):
+        for i in range(self.rows):
+            print(self.matrix[i])
+        print("\n")
 
-    def createMatrix(s, p):
-        return Matrix(s, p).matrix
-
-    def display(matrix):
-        string = len(matrix)
-        for i in range(string):
-            print(matrix[i])
-        print(" ")
-
-    def add(p1, p2):
-        string = len(p1)
-        pillar = len(p1[0])
-        matrix = []
-        if string == len(p2) and pillar == len(p2[0]):
-            for i in range(string):
-                matrix.append([])
-                for j in range(pillar):
-                    matrix[i].append(p1[i][j] + p2[i][j])
+    def __add__(self, other):
+        if (self.rows == other.rows) and (self.columns == other.columns):
+            result = Matrix(self.rows, self.columns)
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    result.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+            return result
         else:
-            print("Can not added")
+            print("Can not be folded!")
             return 1
-        return matrix
-    def sub(p1, p2):
-        string = len(p1)
-        pillar = len(p1[0])
-        matrix = []
-        if string == len(p2) and pillar == len(p2[0]):
-            for i in range(string):
-                matrix.append([])
-                for j in range(pillar):
-                    matrix[i].append(p1[i][j] - p2[i][j])
-        else:
-            print("Can not subbed")
-            return 1
-        return matrix
-    def compOfNumber(matrix, n):
-        result = []
-        for i in range(len(matrix)):
-            result.append([])
-            for j in range(len(matrix[0])):
-                result[i].append(matrix[i][j] * n)
-        return result
 
-    def compTwoMatrix(p1, p2):
-        result = []
-        resultS = len(p1)
-        resultP = len(p2[0])
-        pillar = len(p1[0])
-        string = len(p2)
-        if pillar == string:
-            for i in range(resultS):
-                result.append([])
-                for j in range(resultP):
+    def __sub__(self, other):
+        if (self.rows == other.rows) and (self.columns == other.columns):
+            result = Matrix(self.rows, self.columns)
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    result.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
+            return result
+        else:
+            print("Can not be folded!")
+            return 1
+
+    def __mul__(self, other):
+        if type(other).__name__ == 'int':
+            result = Matrix(self.rows, self.columns)
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    result.matrix[i][j] = self.matrix[i][j]*other
+            return result
+        elif self.columns == other.rows:
+            result = Matrix(self.rows, other.columns)
+            for i in range(self.rows):
+                for j in range(other.columns):
                     element = 0
-                    for k in range(pillar):
-                        element += p1[i][k]*p2[k][j]
-                    result[i].append(element)
+                    for k in range(self.columns):
+                        element += self.matrix[i][k]*other.matrix[k][j]
+                    result.matrix[i][j] = element
             return result
         else:
             print("Can not composition")
             return 1
 
-    def transMatrix(matrix):
-        result = []
-        pillar = len(matrix[0])
-        string = len(matrix)
-        for j in range(pillar):
-            result.append([])
-            for i in range(string):
-                result[j].append(matrix[i][j])
+    def trans(self):
+        result = Matrix(self.rows, self.columns)
+        for j in range(self.columns):
+            for i in range(self.rows):
+                result.matrix[j][i] = self.matrix[i][j]
+        self.matrix = result.matrix
+
+    def getmatrix(self, r):
+        result = Matrix(self.rows - 1, self.columns - 1)
+        k = 0
+        for i in range(self.rows):
+            result.matrix[k] = []
+            if i != r:
+                for j in range(self.columns):
+                    k += 1
+                    if j != 0:
+                        result.matrix[k].append(self.matrix[i][j])
         return result
 
-    def detMatrix(matrix):
-        det = 0
-        string = len(matrix)
-        pillar = len(matrix[0])
-        if string == pillar:
-            if string == 1:
-                det += matrix[0][0]
-            elif string == 2:
-                det += matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-            elif string == 3:
-                det += matrix[0][0]*matrix[1][1]*matrix[2][2] + matrix[1][0]*matrix[0][2]*matrix[2][1] + matrix[0][1]*matrix[1][2]*matrix[2][0]
-                det -= (matrix[0][2]*matrix[1][1]*matrix[2][0] + matrix[0][0]*matrix[1][2]*matrix[2][1] + matrix[0][1]*matrix[2][2]*matrix[1][0])
+    def det(self):
+        if self.rows == self.columns:
+            if self.rows == 3:
+                det = 0
+                det += self.matrix[0][0] * self.matrix[1][1] * self.matrix[2][2] + self.matrix[1][0] * self.matrix[0][2] * self.matrix[2][1] + \
+                       self.matrix[0][1] * self.matrix[1][2] * self.matrix[2][0]
+                det -= (self.matrix[0][2] * self.matrix[1][1] * self.matrix[2][0] + self.matrix[0][0] * self.matrix[1][2] * self.matrix[2][1] +
+                        self.matrix[0][1] * self.matrix[2][2] * self.matrix[1][0])
+                return det
             else:
-                print("Can not DET")
-                det += 1
-            return det
+                det = 0
+                for i in range(self.rows):
+                    if i % 2 == 0:
+                        det += self.matrix[i][0] * self.getmatrix(i).det()
+                    else:
+                        det += (-self.matrix[i][0] * self.getmatrix(i).det())
+                return det
         else:
             print("Can not det")
-            return 1
+            return 'string'
